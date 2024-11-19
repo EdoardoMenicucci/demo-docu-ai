8a7df34822cc404028b06ca025734e6487f55fa8
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-11-19 20:31:21 +0100

    miglioramento info

--- components/OtherInfo.vue
+++ components/OtherInfo.vue
@@ -1,13 +1,12 @@
 <template>
-  <h1 class="text-4xl font-extralight mt-4">Altre Info</h1>
-  <p class="text-center font-extralight m-1">-La vostra API KEY verra inviata dal vosto Computer direttamente
-    all'API
-    di Gemini e non verra tracciata ne passera' nel attraverso il mio server-</p>
-  <p class="text-center font-extralight m-1">-La vostra API KEY non verra' salvata nel Local Storage per motivi di
-    sicurezza ma nel Session Storage e alla chiusura della tab o del browser verra eliminata-</p>
-  <p class="text-center font-extralight m-1">-In questa versione non e' presente alcuna autenticazione, nessun
-    server backend / API, per questo motivo l'utilizzo di quest'applicazione e' completamente gratuito ma offre
-    funzionalita' limitate.-
+  <h1 class="text-4xl font-extralight mt-4">Altre Informazioni</h1>
+  <p class="text-center font-extralight m-1">-La tua API KEY verrà inviata direttamente dal tuo computer all'API di
+    Gemini. Non verrà mai tracciata o trasmessa attraverso il mio server. -</p>
+  <p class="text-center font-extralight m-1">- Per motivi di sicurezza, la tua API KEY non verrà salvata nel Local
+    Storage. Sarà invece memorizzata nel Session Storage e verrà eliminata automaticamente alla chiusura della scheda o
+    del browser. -</p>
+  <p class="text-center font-extralight m-1">- Questa versione non include funzionalità di autenticazione, server
+    backend o API. Per questo motivo, l'applicazione è completamente gratuita ma offre funzionalità limitate. -
   </p>
 </template>
 
